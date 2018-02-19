c4f1762386d891b8bb974e1b11e2958084e767d1
Konstantinos Norgias <kostanorgias@gmail.com>
2018-02-19 17:00:09 +0100

    Synced ENUMS with back-end, minor changes, GET request for my-days-overview

--- js/my-scripting.js
+++ js/my-scripting.js
@@ -12,3 +12,31 @@ function testRequest() {
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip(); 
 });
+
+ //     function ajaxData(typeRequest, url, callback, objectToSend){
+    // function getOvierview(){
+    //     var data={};
+    //     data.username = document.getElementById("username").value;
+    //     data.password = document.getElementById("password").value;
+    //     data = JSON.stringify(data);
+    //     $.ajax({
+    //         url: "http://localhost:8083/login",
+    //         data: data,
+    //         type: "POST",
+    //         beforeSend: function(xhr){xhr.setRequestHeader('Content-type', 'application/json');},
+    //         success: function(data) { console.log(data); }
+    //     });
+    // }
+    function getOvierview(){
+        var workdays={};
+        $.ajax({
+            url: "http://localhost:8083//employee/1/overviewHours",
+            workdays: workdays,
+            type: "GET",
+            beforeSend: function(xhr){xhr.setRequestHeader('Content-type', 'application/json');},
+            success: function(workdays) { 
+                console.log(workdays);
+
+            }
+        });
+    }
