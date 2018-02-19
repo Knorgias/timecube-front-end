d7c77da64ff56ce33157bedddb7451f0d66e0b33
Konstantinos Norgias <kostanorgias@gmail.com>
2018-02-19 10:54:07 +0100

    Minor changes to elements, added code in my-scripting.js

---
+++ js/my-scripting.js
@@ -0,0 +1,10 @@
+// My test GET request to the back-end
+function testRequest() {
+    console.log("I'm alive!");
+    // var xhttp = new XMLHttpRequest();
+    // xhttp.onreadystatechange = function() {
+    //     alert(this.responseText);
+    // };
+    // xhttp.open("GET", "ajax_info.txt", true);
+    // xhttp.send();
+  }
